878fe3cbcdb447d1df673938560354f2fc62ad10
WIng57 <ddariusd007@gmail.com>
2022-11-03 23:26:49 -0400

    Adding trajectory constants and that

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import java.nio.file.Path;
+
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.Filesystem;
+
 public final class Constants {
 
   // *****************************************
@@ -24,9 +29,38 @@ public final class Constants {
   public static final double kCountsPerRevolution = 1440.0;
   public static final double kWheelDiameterInch = 2.75591; // 70 mm
 
+  /////////////// SYSID VALUES ///////////////
+
+  public static final double ksVolts = 0.558;
+  public static final double kvVoltSecondsPerMeter = 3.4335;
+  public static final double kaVoltSecondsSquaredPerMeter = 0.171;
+
+  public static final double kPDriveVel = 3.2137;
+
+  //TODO: Find real values for all of these innit
+  public static final double kMaxVelocityMetersPerSecond = 0.0;
+  public static final double kMaxAccelerationMetersPerSecondSquared = 0.0;
+
+  public static final double kTrackwidthMeters = 0.74551;
+    public static final DifferentialDriveKinematics kDriveKinematics =
+        new DifferentialDriveKinematics(kTrackwidthMeters);
+  
+  /////////////// AUTO STUFF ///////////////
+
+  // Reasonable baseline values for a RAMSETE follower in units of meters and
+  // seconds
+  public static final double kRamseteB = 2;
+  public static final double kRamseteZeta = 0.7;
+  
   // *****************************************
   // ************** JOYSTICKS ****************
   // *****************************************
 
   public static final int DRIVERCONTROLLER = 3;
+
+  // *****************************************
+  // ************** OTHER IDC ****************
+  // *****************************************
+
+  public static final Path DEPLOY_DIRECTORY = Filesystem.getDeployDirectory().toPath();
 }
